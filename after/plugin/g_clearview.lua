270fe708c0bfa38bbf3b08abf69f157536800688
brennan-mint <markbrennanlicon39@gmail.com>
2026-02-27 10:11:00 +0800

    fixed issue: RemoveView function not working as intended because of local variable file not being inside the scope of RemoveView

--- after/plugin/g_clearview.lua
+++ after/plugin/g_clearview.lua
@@ -3,19 +3,18 @@
 -- GLOBAL
 -- Removes view file of current buffer directory "stdpath("state")/view/"
 --
-local file = vim.api.nvim_buf_get_name(0)
 
 function RemoveView()
 
+	local file = vim.api.nvim_buf_get_name(0)
 	local currentBufName	= string.gsub(file, "/(%w+)/(%w+)(%p)(%w+)", "", 1)
 	local viewPath	= vim.fn.stdpath("state").."/view/"
 	local viewFile	= function()
-		local p1	=	string.gsub(currentBufName, "^%p", "~/")
-		local res	=	string.gsub(p1, "/", "=+").."="
-		return res
+		local p1	=	string.gsub(currentBufName, "^%p", "~/", 1)
+		return string.gsub(p1, "/", "=+").."="
 	end
 
 	os.remove(viewPath..viewFile())
-
+	print(viewPath..viewFile())
 end
 
